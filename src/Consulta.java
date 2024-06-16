998fdbc73ef4bdf3a5b7f60221bdc783500524cd
isasa <129801545+isa-sa@users.noreply.github.com>
2024-06-16 17:16:12 -0300

    Update Consulta.java

--- src/Consulta.java
+++ src/Consulta.java
@@ -20,7 +20,7 @@ public class Consulta {
                     .send(request, HttpResponse.BodyHandlers.ofString());
             return new Gson().fromJson(response.body(), Endereco.class);
         } catch (Exception e) {
-            throw new RuntimeException("Não consegui obter o endereço a partir desse CEP.");
+            throw new RuntimeException("Não foi possível obter o endereço a partir desse CEP.");
         }
 
     }
